51f270860d94a1323bee0006840597f8f233198f
Kevin Tran <kevin115533@yahoo.com>
2021-02-11 21:26:18 -0600

    week 5 lab

--- ITD3333Week5/Default.aspx
+++ ITD3333Week5/Default.aspx
@@ -6,11 +6,7 @@
 <head runat="server">
     <title></title>
 </head>
-<style>
-    body{
-        line-height: 2;
-    }
-</style>
+
 <body style="height: 104px"  >
     <form id="form1" runat="server">
         <div>
@@ -20,13 +16,23 @@
         <div>
             <asp:Label ID="lastLbl" runat="server" Text="Last Name:"></asp:Label>
             <asp:TextBox ID="lastTxtbox" runat="server"></asp:TextBox>
+            
+        </div>
+        <div>
+            <asp:Label ID="postNameLbl" runat="server" Text="Postback Name: " ></asp:Label>
+            <asp:Label ID="postedNameLbl" runat="server" ></asp:Label>
         </div>
         <div>
             <asp:Label ID="postLbl" runat="server" Text="Post counter:"></asp:Label>
-            <asp:Label ID="postCountLbl" runat="server" ></asp:Label>
+            <asp:Label ID="postCountLbl" runat="server" Text ='<%# postCounter%>' ></asp:Label>
         </div>    
-        <asp:Button ID="postBtn" runat="server" Text="Post page" OnClick="cmdPostback_Click"  />
+        <asp:Button ID="postBtn" runat="server" Text="Post page"  OnClick="cmdPostback_Click"  />
     </form>
 </body>
 </html>
 
+<style>
+    body{
+        line-height: 2;
+    }
+</style>
--- ITD3333Week5/Default.aspx.cs
+++ ITD3333Week5/Default.aspx.cs
@@ -7,24 +7,59 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    public int postCounter = 0;
     protected void Page_Load(object sender, EventArgs e)
     {
+        postCountLbl.Text = postCounter.ToString();
+        postCountLbl.ForeColor = System.Drawing.Color.Red;
+        if (this.IsPostBack)
+        {
+            
+        }
+    }
 
+    protected void Page_PreRender(Object sender, EventArgs e)
+    {
+        //ViewState["user"] = user;
     }
 
     protected void cmdPostback_Click(object sender, EventArgs e)
     {
-        int counter;
+        
         if (ViewState["Counter"] == null)
         {
-            counter = 1;
+            postCounter = 1;
         }
         else
         {
-            counter = (int)ViewState["Counter"] + 1;
+            postCounter = (int)ViewState["Counter"] + 1;
         }
-        ViewState["Counter"] = counter;
-        postCountLbl.Text = counter.ToString();
+        ViewState["Counter"] = postCounter;
+        postCountLbl.Text = postCounter.ToString();
         postCountLbl.ForeColor = System.Drawing.Color.Red;
+        UserName user = new UserName(firstTxtbox.ToString(), lastTxtbox.ToString());
+        user = (UserName)ViewState["user"];
+    }
+
+    [Serializable]
+    public class UserName
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public UserName(string first, string last)
+        {
+            FirstName = first;
+            LastName = last;
+        }
     }
 }
